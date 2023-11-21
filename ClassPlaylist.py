9e3f5622cb24066c8ac8121dfd4e69d2a396ca2b
ArianaBlancoF <122564390+ArianaBlancoF@users.noreply.github.com>
2023-11-20 23:38:14 -0400

    Add files via upload

--- ClassPlaylist.py
+++ ClassPlaylist.py
@@ -6,22 +6,36 @@ class Playlist():
         self.canciones = []
         self.indice_actual = 0 
         self.bucle = False 
+        self.reproducciones = 0
+        self.historial = []
 
 
 
     def cargar_playlist(self):
-        with open('Playlist.txt', 'r') as f:
-            archivo = f.readline()
+        try:
+            with open('Playlist.txt', 'r') as f:
+                archivo = f.readlines() 
             for linea in archivo:
                 datos = linea.strip().split(",")
                 cancion = Cancion(datos[0], datos[1], datos[2], datos[3], datos[4], datos[5])
                 self.canciones.append(cancion)
-    
+        except IOError:
+            print("No se pudo abrir o leer el archivo")
 
 
-    def agregar_cancion(self, num_cancion, nombre, artista, num_reproduccion, año, idioma):    #DUDAS_REVISAR/////
+
+
+    def agregar_cancion(self, num_cancion, nombre, artista, num_reproduccion, año, idioma):
+        """
+        Agrega una nueva canción a la playlist y la guarda en el archivo de txt.
+        """
         cancion = Cancion(num_cancion, nombre, artista, num_reproduccion, año, idioma)
         self.canciones.append(cancion)
+        with open('Playlist.txt', 'w') as f:
+            for cancion in self.canciones:
+                f.write(f"{cancion.num_cancion},{cancion.nombre},{cancion.artista},{cancion.num_reproduccion},{cancion.año},{cancion.idioma}\n")
+        print("Nueva canción agregada con éxito")
+
 
 
 
@@ -30,6 +44,7 @@ class Playlist():
             if cancion.nombre == nombre:
                 return cancion
         return None
+
 
 
 
@@ -42,36 +57,66 @@ class Playlist():
 
 
 
+
     def reproducir_cancion(self):
         cancion = self.canciones[self.indice_actual]
         print(f"Reproduciendo: {cancion.nombre} - {cancion.artista}")
         self.indice_actual += 1
+        self.historial.append(cancion) 
+
 
     def reproducir(self):
-        while True:
-            if self.bucle:
-                if self.indice_actual >= len(self.canciones):
-                    self.indice_actual = 0
-                    random.shuffle(self.canciones) # mezcla la lista de canciones
-                    print("Se ha reproducido toda la playlist. Mezclando canciones...")
-                self.reproducir_cancion()
-            else:
-                if self.indice_actual >= len(self.canciones):
-                    print("Fin de la reproducción.")
-                    break
-                self.reproducir_cancion()
+        self.reproduciendo = True # activar la reproducción
+        if self.indice_actual >= len(self.canciones): 
+            print("Fin de la reproducción.") 
+            self.reproduciendo = False 
+            if self.bucle: 
+                random.shuffle(self.canciones) 
+                self.reproducciones += 1 # incrementar el contador de reproducciones
+                print(f"Se ha reproducido la playlist {self.reproducciones} veces. Empezando de nuevo...") 
+                self.indice_actual = 0 # volver al principio de la lista        
+        else: 
+            if self.bucle: # si el modo bucle está activado
+                random.shuffle(self.canciones) # mezcla la lista de canciones
+                print("Mezclando canciones...")         
+            self.reproducir_cancion() # reproducir la canción actual
+
 
     def control_reproduccion(self, accion):
-        if accion == 0: # Canción anterior
-            if self.indice_actual > 0:
+        if accion == 0: # Canción anterior    ////////  ESTA ACCIÓN NO CORRE ////// === NO SÉ COMO ARREGLAR EL ERROR
+            if self.indice_actual > 0: 
                 self.indice_actual -= 1
-        elif accion == 1: # Canción siguiente
-            if self.indice_actual < len(self.canciones) - 1:
+                self.reproducir_cancion() 
+            else:
+                print("No hay ninguna canción anterior por el momento.")
+        elif accion == 1: # Canción siguiente    //////////   ESTA TAMPOCO CORRE ///////====
+            if self.indice_actual < len(self.canciones) - 1: 
                 self.indice_actual += 1
+                self.reproducir_cancion() 
+            else: 
+                self.indice_actual = 0
+                print("La última canción de la lista ya se está reproduciendo.")
         elif accion == 2: # Bucle
-            self.bucle = True
+            self.bucle = True 
+            print("Se ha activado el modo bucle.") 
         elif accion == 3: # Eliminar Bucle
-            self.bucle = False
+            if self.bucle: 
+                self.bucle = False 
+                print("Se ha desactivado el modo bucle.") 
+            else:
+                print("El modo bucle no está activado.") 
+
+
+
+
+    def contar_canciones_historial(self):
+        conteo = {} # Diccionario vacío para almacenar el conteo de canciones
+        for cancion in self.historial:
+            if cancion.nombre in conteo:
+                conteo[cancion.nombre] += 1
+            else:
+                conteo[cancion.nombre] = 1
+        return conteo
 
 
 
@@ -79,6 +124,108 @@ class Playlist():
         for cancion in self.canciones:
             cancion.mostrar()
             print()
+
+
+
+
+    # NO ENTIENDO POR QUÉ NO IMPRIME NADA EL ORDENAR///////=======
+    def burbuja_creciente_numero(self):
+        n = len(self.canciones)
+        intercambiado = True # inicializar la variable intercambiado a True
+        while intercambiado: # mientras haya algún intercambio
+            intercambiado = False 
+            for j in range(0, n-1): 
+                if self.canciones[j].num_cancion > self.canciones[j + 1].num_cancion: 
+                    self.canciones[j], self.canciones[j + 1] = self.canciones[j + 1], self.canciones[j] 
+                    intercambiado = True # marcar que se ha hecho un intercambio
+            n -= 1 # reducir el tamaño de la lista a ordenar en uno
+
+    def burbuja_decreciente_numero(self):
+        n = len(self.canciones)
+        intercambiado = True 
+        while intercambiado: 
+            intercambiado = False 
+            for j in range(0, n-1): 
+                if self.canciones[j].num_cancion < self.canciones[j + 1].num_cancion: 
+                    self.canciones[j], self.canciones[j + 1] = self.canciones[j + 1], self.canciones[j] 
+                    intercambiado = True 
+            n -= 1 
+
+    def ordenar_por_numero(self, orden):
+        if orden == 0 : # si el orden es creciente
+            self.burbuja_creciente_numero() 
+        elif orden == 1: # si el orden es decreciente
+            self.burbuja_decreciente_numero()
+        else: 
+            print("Orden inválido") 
+
+
+
+
+    def burbuja_creciente_reproducciones(self):
+        n = len(self.canciones)
+        intercambiado = True # inicializar la variable intercambiado a True
+        while intercambiado: # mientras haya algún intercambio
+            intercambiado = False 
+            for j in range(0, n-1): 
+                if self.canciones[j].num_reproducciones > self.canciones[j + 1].num_reproducciones: 
+                    self.canciones[j], self.canciones[j + 1] = self.canciones[j + 1], self.canciones[j] 
+                    intercambiado = True # marcar que se ha hecho un intercambio
+            n -= 1 # reducir el tamaño de la lista a ordenar en uno
+
+    def burbuja_decreciente_reproducciones(self):
+        n = len(self.canciones)
+        intercambiado = True 
+        while intercambiado: 
+            intercambiado = False 
+            for j in range(0, n-1): 
+                if self.canciones[j].num_reproducciones < self.canciones[j + 1].num_reproducciones: 
+                    self.canciones[j], self.canciones[j + 1] = self.canciones[j + 1], self.canciones[j] 
+                    intercambiado = True 
+            n -= 1 
+
+    def ordenar_por_reproducciones(self, orden):
+        if orden == 0 : # si el orden es creciente
+            self.burbuja_creciente_reproducciones() # ordenar la lista de canciones por el número de reproducciones de forma creciente usando el método burbuja
+        elif orden == 1: # si el orden es decreciente
+            self.burbuja_decreciente_reproducciones() # ordenar la lista de canciones por el número de reproducciones de forma decreciente usando el método burbuja
+        else: # si el orden no es ni creciente ni decreciente
+            print("Orden inválido") # imprimir un mensaje de orden inválido
+
+
+
+    
+    
+    def burbuja_creciente_nombre(self):
+        n = len(self.canciones)
+        intercambiado = True # inicializar la variable intercambiado a True
+        while intercambiado: # mientras haya algún intercambio
+            intercambiado = False 
+            for j in range(0, n-1): 
+                if self.canciones[j].nombre > self.canciones[j + 1].nombre: # si el nombre de la canción actual es mayor alfabéticamente que el de la siguiente
+                    self.canciones[j], self.canciones[j + 1] = self.canciones[j + 1], self.canciones[j] # intercambiar las canciones
+                    intercambiado = True # marcar que se ha hecho un intercambio
+            n -= 1 # reducir el tamaño de la lista a ordenar en uno
+
+    def burbuja_decreciente_nombre(self):
+        n = len(self.canciones)
+        intercambiado = True 
+        while intercambiado: 
+            intercambiado = False 
+            for j in range(0, n-1): 
+                if self.canciones[j].nombre < self.canciones[j + 1].nombre: # si el nombre de la canción actual es menor alfabéticamente que el de la siguiente
+                    self.canciones[j], self.canciones[j + 1] = self.canciones[j + 1], self.canciones[j] # intercambiar las canciones
+                    intercambiado = True 
+            n -= 1 
+
+    def ordenar_por_nombre(self, orden):
+        if orden == 0 : # si el orden es creciente
+            self.burbuja_creciente_nombre() # ordenar la lista de canciones por el nombre de forma creciente usando el método burbuja
+        elif orden == 1: # si el orden es decreciente
+            self.burbuja_decreciente_nombre() # ordenar la lista de canciones por el nombre de forma decreciente usando el método burbuja
+        else: # si el orden no es ni creciente ni decreciente
+            print("Orden inválido") # imprimir un mensaje de orden inválido
+
 
 
 
---
+++ main.py
@@ -0,0 +1,238 @@
+from ClassPlaylist import *
+import re
+
+mi_playlist = Playlist()
+mi_playlist.cargar_playlist()
+
+
+while True:
+
+    print('''
+                     Bienvenido a Spotify 
+                      Playlist Manager
+          ''')
+    print("------------------------------------------")
+    print("Menú:")
+    print("1. Agregar una canción a la playlist")
+    print("2. Buscar una canción por su nombre")
+    print("3. Filtrar por artista")
+    print("4. Reproducir la playlist")
+    print("5. Ordenar por número de canción")
+    print("6. Ordenar por número de reproducciones")
+    print("7. Ordenar por nombre de canción")
+    print("8. Mostrar la playlist")
+    print("9. Mostrar el historial y conteo de canciones escuchadas")
+    print("0. Salir")
+    print("-------------------------------------------")
+
+    opcion = input("Ingrese una opción: ")
+
+
+    if opcion == "1":
+        while True:
+            try:
+                num_cancion = int(input("Ingrese el número de la canción: "))
+                if num_cancion >= 16:
+                    break
+                else:
+                    print("El número de la canción debe ser un número entero a partir de 16")
+            except ValueError:
+                print("El número de la canción debe ser un número entero")
+
+        while True:
+            nombre = input("Ingrese el nombre de la canción: ")
+            if nombre != "" and re.match("[A-Za-z ]+", nombre):
+                break
+            else:
+                print("El nombre de la canción debe ser una cadena de caracteres alfabéticos y no puede estar vacío")
+
+        while True:
+            artista = input("Ingrese el nombre del artista: ")
+            if artista != "" and re.match("[A-Za-z ]+", artista):
+                break
+            else:
+                print("El nombre del artista debe ser una cadena de caracteres alfabéticos y no puede estar vacío")
+
+        while True:
+            try:
+                num_reproduccion = int(input("Ingrese el número de reproducciones: "))
+                if num_reproduccion > 0:
+                    break
+                else:
+                    print("El número de reproducciones debe ser un número entero positivo")
+            except ValueError:
+                print("El número de reproducciones debe ser un número entero")
+
+        while True:
+            try:
+                año = int(input("Ingrese el año de lanzamiento: "))
+                if año > 0:
+                    break
+                else:
+                    print("El año de lanzamiento debe ser un número entero positivo")
+            except ValueError:
+                print("El año de lanzamiento debe ser un número entero")
+
+        while True:
+            idioma = input("Ingrese el idioma de la canción: ")
+            if re.match("[A-Za-z]+", idioma):
+                break
+            else:
+                print("El idioma de la canción debe ser una cadena de caracteres alfabéticos")
+
+        mi_playlist.agregar_cancion(num_cancion, nombre, artista, num_reproduccion, año, idioma)
+
+
+
+    elif opcion == "2":
+        nombre = input("Ingrese el nombre de la canción: ").lower().title()
+        if nombre == "":
+            print("El nombre de la canción no puede estar vacío")
+        else:
+            # La expresión regular [A-Za-z ]+ significa una o más letras de la A a la Z, mayúsculas o minúsculas, o espacios
+            if re.match("[A-Za-z ]+", nombre):
+                cancion = mi_playlist.buscar_cancion(nombre)
+                if cancion is not None:
+                    print("Canción encontrada:")
+                    cancion.mostrar()
+                else:
+                    print("No se encontró la canción")
+            else:
+                print("El nombre de la canción no es válido")
+
+
+
+    elif opcion == "3":
+        artista = input("Ingrese el nombre del artista: ").lower().title()
+        if artista == "":
+            print("El nombre del artista no puede estar vacío")
+        else:
+            # La expresión regular [A-Za-z ]+ significa una o más letras de la A a la Z, mayúsculas o minúsculas, o espacios
+            if re.match("[A-Za-z ]+", artista):
+                canciones_por_artista = mi_playlist.filtrar_por_artista(artista)
+                if len(canciones_por_artista) > 0:
+                    print("Canciones del artista:")
+                    for cancion in canciones_por_artista:
+                        cancion.mostrar()
+                else:
+                    print("No se encontraron canciones del artista")
+            else:
+                print("El nombre del artista no es válido")
+
+    
+
+
+    elif opcion == "4":
+        while True: # crear un bucle infinito
+            print("<><><><><><><><><><><><><")
+            print("Menú de reproducciones:")
+            print("1. Reproducir playlist")
+            print("2. Canción anterior")
+            print("3. Siguiente canción")
+            print("4. Activar bucle")
+            print("5. Desactivar bucle")
+            print("6. Salir")
+            print("<><><><><><><><><><><><><")
+            opcion = input("Elige una opción: ") 
+            if opcion == "1": 
+                mi_playlist.reproducir() # reproducir la playlist
+            elif opcion == "2": 
+                mi_playlist.control_reproduccion(0) # reproducir la canción anterior
+            elif opcion == "3": 
+                mi_playlist.control_reproduccion(1) # reproducir la canción siguiente
+            elif opcion == "4":
+                mi_playlist.control_reproduccion(2) # activar el modo bucle
+            elif opcion == "5": 
+                mi_playlist.control_reproduccion(3) # desactivar el modo bucle
+            elif opcion == "6": 
+                print("Gracias por usar el reproductor de música. Hasta pronto.") 
+                break # salir del bucle y terminar el programa
+            else: 
+                print("Por favor, elige una opción válida.") 
+    
+
+
+    elif opcion == "5":
+        while True: # crear un bucle infinito
+            print("Orden por número de canción:")
+            print("1. Creciente")
+            print("2. Decreciente")
+            print("3. Salir")
+            opcion = input("Elige una opción: ") 
+            if opcion == "1": 
+                mi_playlist.ordenar_por_numero(0) # reproducir la playlist
+            elif opcion == "2": 
+                mi_playlist.ordenar_por_numero(1) # reproducir la canción anterior
+            elif opcion == "3": 
+                print("Volver al menú.") 
+                break # salir del bucle y terminar el programa
+            else: 
+                print("Por favor, elige una opción válida.") 
+    
+
+    elif opcion == "6":
+        while True: # crear un bucle infinito
+            print("Orden por reproducciones:")
+            print("1. Creciente")
+            print("2. Decreciente")
+            print("3. Salir")
+            opcion = input("Elige una opción: ") 
+            if opcion == "1": 
+                mi_playlist.ordenar_por_reproducciones(0) # reproducir la playlist
+            elif opcion == "2": 
+                mi_playlist.ordenar_por_reproducciones(1) # reproducir la canción anterior
+            elif opcion == "3": 
+                print("Volver al menú.") 
+                break # salir del bucle y terminar el programa
+            else: 
+                print("Por favor, elige una opción válida.") 
+    
+    
+    elif opcion == "7":
+        while True: # crear un bucle infinito
+            print("Orden por canción:")
+            print("1. Creciente")
+            print("2. Decreciente")
+            print("3. Salir")
+            opcion = input("Elige una opción: ") 
+            if opcion == "1": 
+                mi_playlist.ordenar_por_nombre(0) # reproducir la playlist
+            elif opcion == "2": 
+                mi_playlist.ordenar_por_nombre(1) # reproducir la canción anterior
+            elif opcion == "3": 
+                print("Volver al menú.") 
+                break # salir del bucle y terminar el programa
+            else: 
+                print("Por favor, elige una opción válida.")         
+    
+
+    elif opcion == "8":
+        mi_playlist.mostrar_playlist()
+
+
+    elif opcion == "9":
+        for nombre, veces in mi_playlist.contar_canciones_historial().items():
+            print(f"> {nombre}: {veces}")
+
+    elif opcion == "0":
+        print("Hasta la próxima.")
+        break
+    else:
+        print("Opción no válida. Intente de nuevo.")        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
